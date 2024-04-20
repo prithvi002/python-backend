e22d2db11ff89b3992a1d199b901c5ba2b75b949
prithvi002 <prithvik@vt.edu>
2024-04-20 15:15:23 -0400

    file into folder

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import PyPDF2
@@ -19,12 +21,28 @@ def get_pdf_data():
         return jsonify({'error': 'No file part'})
 
     file = request.files['file']
+    if file:
+    # Folder path where the PDF will be stored
+        folder_path = './files'  # Using relative path for the folder
+
+    # Check if the folder exists, delete and recreate if it does
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)  # Remove the folder and all its contents
+    os.makedirs(folder_path)  # Create the folder anew
+
+    # Full path to save the file
+    full_path = os.path.join(folder_path, file.filename)
+
+    # Save the file
+    file.save(full_path)
+    
 
     # Check if the file is a PDF
     if file.filename == '' or not file.filename.endswith('.pdf'):
         return jsonify({'error': 'Invalid file. Please upload a PDF file'})
 
     # Read the PDF and extract the first line
+
     
     pdf_reader = PyPDF2.PdfReader(file)
     first_page = pdf_reader.pages[0]
